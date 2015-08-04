afe99ec2d094fd48d730fa0a7c3650f9f46c8872
futoricky <ricardoamercado@gmail.com>
2015-08-04 00:36:16 -0400

    begin unit tests

--- tests/unit/components/drop-zone/component-test.js
+++ tests/unit/components/drop-zone/component-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 
 moduleForComponent('drop-zone', 'Unit | Component | drop zone', {
@@ -16,4 +17,37 @@ test('it renders', function(assert) {
   // Renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
+});
+
+test('that is being initialized', function(assert) {
+  let component = this.subject();
+  this.render();
+  Ember.run(() => {
+    assert.ok(component.myDropzone);
+  });
+});
+
+test('that options are being assigned properly', function(assert){
+  let component = this.subject();
+
+  let urlArr = ['#', 'http://example.com/example', '/here/inside'];
+  component.set('url', urlArr[Math.floor(Math.random() * urlArr.length)]);
+
+  let methodArr = ['POST', 'GET'];
+  component.set('method', methodArr[Math.floor(Math.random() * methodArr.length)]);
+
+  component.set('parallelUploads', Math.floor(Math.random() * 16));  
+
+  component.set('maxFilesize', Math.floor(Math.random() * 10000));
+
+  this.render();
+
+  Ember.run(() => {
+    let dropOption = component.myDropzone.options;
+
+    assert.equal(component.url, dropOption.url);
+    assert.equal(component.method, dropOption.method);
+    assert.equal(component.parallelUploads, dropOption.parallelUploads);
+    assert.equal(component.maxFilesize, dropOption.maxFilesize);
+  });
 });
