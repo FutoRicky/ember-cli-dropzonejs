64ded488004791a0b0237473989afffb1d5f71cd
Oskar <oskarrough@gmail.com>
2016-08-06 12:17:52 +0200

    Support for FastBoot

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ module.exports = {
 
     var options = app.options.emberCliDropzonejs || {includeDropzoneCss: true};
 
-    app.import('bower_components/dropzone/dist/dropzone.js');
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      // This will only be included in the browser build
+      app.import('bower_components/dropzone/dist/dropzone.js');
+    }
 
     if (options.includeDropzoneCss){
       app.import('bower_components/dropzone/dist/dropzone.css');
