138d80b43223ff17d949c52bebf5eb7eceae0448
futoricky <ricardoamercado@gmail.com>
2016-10-09 20:52:21 -0400

    change baseUrl to rootURL. Minor changes to .travis.yml

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -38,7 +38,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.rootUrl = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
@@ -49,7 +49,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/ember-cli-dropzonejs';
+    ENV.rootUrl = '/ember-cli-dropzonejs';
   }
 
   return ENV;
