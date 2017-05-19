de657daab7ba4ace04deca1fdd76b8880e917c99
Jake LeBoeuf <dev@jakeleboeuf.com>
2017-05-19 00:29:33 -0400

    moves css into treeForStyles hook

--- index.js
+++ index.js
@@ -2,36 +2,41 @@
 'use strict';
 
 var path = require('path');
-var Funnel = require('broccoli-funnel');
 var MergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
 var map = require('broccoli-stew').map;
 
 module.exports = {
   name: 'ember-cli-dropzonejs',
 
   treeForVendor(vendorTree) {
-    return new MergeTrees([
-      vendorTree,
-      new Funnel(
-        path.join(this.project.root, 'node_modules', 'dropzone/dist/min'),
-        {
-          files: ['dropzone.min.css']
-        }
-      ),
-      map(
-        new Funnel(
-          path.join(this.project.root, 'node_modules', 'dropzone/dist/min'),
-          {
-            files: ['dropzone.min.js']
-          }
-        ),
-        content => `if (typeof FastBoot === 'undefined') { ${content} }`
-      )
-    ]);
+    var dropzoneJs = new Funnel(
+      path.join(this.project.root, 'node_modules', 'dropzone/dist/min'),
+      { files: ['dropzone.min.js'] }
+    );
+
+    dropzoneJs = map(
+      dropzoneJs,
+      content => `if (typeof FastBoot === 'undefined') { ${content} }`
+    );
+
+    return new MergeTrees(vendorTree, dropzoneJs);
+  },
+
+  treeForStyles(styleTree) {
+    var dropzoneCss = new Funnel(
+      path.join(this.project.root, 'node_modules', 'dropzone/dist/min'),
+      {
+        files: ['dropzone.min.css'],
+        destDir: 'app/styles'
+      }
+    );
+
+    return new MergeTrees(styleTree, dropzoneCss);
   },
 
   included() {
     this.import('vendor/dropzone.min.js');
-    this.import('vendor/dropzone.min.css');
+    this.import('app/styles/dropzone.min.css');
   }
 };
