aa35c04c02c88a3ac4858cb0091ca80d045c1a63
Ricardo Mercado <ricardoamercado@gmail.com>
2018-04-04 16:39:24 -0400

    Update testem.js

--- testem.js
+++ testem.js
@@ -1,5 +1,6 @@
-/* eslint-env node */
+/*jshint node:true*/
 module.exports = {
+  "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
