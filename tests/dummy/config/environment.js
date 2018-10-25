d432184c2fc63edc7a271e781f879d354cb0b9c4
Ricardo Mercado <ricardoamercado@gmail.com>
2018-10-25 15:40:59 -0400

    Upgrade to ember-cli@3.5.0

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,26 +1,26 @@
-/* eslint-env node */
+'use strict';
 
 module.exports = function(environment) {
-  var ENV = {
+  let ENV = {
     modulePrefix: 'dummy',
-    environment: environment,
+    environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
-    },
-
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
     },
     contentSecurityPolicy: {
       'default-src': "'none' *",
-      },
-      EXTEND_PROTOTYPES: {
+    },
+
+    EXTEND_PROTOTYPES: {
       // Prevent Ember Data from overriding Date.parse.
       Date: false,
       'script-src': "'self' 'unsafe-eval' *",
@@ -29,10 +29,16 @@ module.exports = function(environment) {
       'img-src': "'self' *",
       'style-src': "'self' 'unsafe-inline' *",
       'media-src': "'self' *"
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
     }
   };
 
   if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
@@ -41,7 +47,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.rootUrl = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
@@ -49,10 +54,11 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.autoboot = false;
   }
 
   if (environment === 'production') {
-    ENV.rootUrl = '/ember-cli-dropzonejs';
+    // here you can enable a production-specific feature
   }
 
   return ENV;
